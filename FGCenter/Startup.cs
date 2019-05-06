f2cf787078eeb9b9c750762dfb0518d6e09c7f99
Ashwin Prakash <ashp94@gmail.com>
2019-05-06 13:34:59 -0500

    fixed login verification

--- FGCenter/Startup.cs
+++ FGCenter/Startup.cs
@@ -74,7 +74,7 @@ namespace FGCenter
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Games}/{action=Index}/{id?}");
             });
         }
     }
